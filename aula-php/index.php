0fb289758299f2a87527a521697325c7f9823716
Willams <84189241+willams502@users.noreply.github.com>
2022-07-15 21:12:24 -0300

    Add files via upload

---
+++ aula-php/index.php
@@ -0,0 +1,12 @@
+<?php 
+    // tipos de saida echo - print
+    //com aspas duplas ele entende como uma variável. Sempre usar aspas duplas para retorno com echo
+    //concatenar .$nome.$sobrenome
+    //print é igual o echo, porem é mais lento. Sempre use o echo.
+    
+    $nome = "Dimitri";
+    $sobrenome = "Teixeira";
+    
+    print $nome;
+    echo "O nome do usuário é $nome $sobrenome";
+?>
